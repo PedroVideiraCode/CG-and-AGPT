b3e42c4f1ead7cd1e19fdbb2e1daa31c3a6a9423
PedroVideiraCode <pedrovideira.work@gmail.com>
2024-10-23 12:33:50 +0100

    100 MB

--- CPP_Demo/CPP_Demo/Vector.cpp
+++ CPP_Demo/CPP_Demo/Vector.cpp
@@ -1,5 +1,6 @@
 #include "Vector.hpp"
 #include <stdexcept>
+#include "Container.hpp"
 
 using namespace std;
 
@@ -10,10 +11,33 @@ Vector::Vector(int s) {
     memset(elem, 0, sz*sizeof(double));
 }
 
-Vector::~Vector()
+Vector::Vector(const Vector& a) // copy constructor
+    :elem{ new double[a.sz] }, // allocate space for elements
+    sz{ a.sz }
 {
-    delete[] elem;
+    for (int i = 0; i != sz; ++i) // copy elements
+        elem[i] = a.elem[i];
 }
+
+Vector::Vector(Vector&& a)
+    :elem{ a.elem }, // "grab the elements" from a
+    sz{ a.sz }
+{
+    a.elem = nullptr; // now a has no elements
+    a.sz = 0;
+}
+
+Vector& Vector::operator=(const Vector& a) // copy assignment
+{
+    double* p = new double[a.sz];
+    for (int i = 0; i != a.sz; ++i)
+        p[i] = a.elem[i];
+    delete[] elem;      // delete old elements
+    elem = p;
+    sz = a.sz;
+    return *this;
+}
+
 
 double& Vector::operator[](int i)
 {
